788d2edf536d1d613b8433f0c5555aae1ce82b28
Gilho <gilhokim0924@gmail.com>
2025-08-01 10:05:31 +0900

    Pages Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Diary from "./pages/Diary";
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/diary" element={<Diary />} />
       </Routes>
     </BrowserRouter>
   )
