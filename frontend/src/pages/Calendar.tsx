82e2b6dbbe5ead040105374f695d7edf0dd3eaca
Gilho <gilhokim0924@gmail.com>
2025-08-11 21:46:38 +0900

    Dashboard Calendar V1

--- frontend/src/pages/Calendar.tsx
+++ frontend/src/pages/Calendar.tsx
@@ -1,16 +1,39 @@
-import react from 'react';
 import Navbar from '../components/Navbar';
+import CalendarUnit from 'react-calendar'
+import {ChevronLeft, ChevronRight} from 'lucide-react';
+import '../css/Calendar.css';
 
 const Calendar: React.FC = () => {
   return (
     <>
       <Navbar />
-      <div className="calendar-page">
-        <div className="calendar-content">
-          <h1>Calendar Page</h1>
-          {/* Additional calendar content can go here */}
+      <section className="calendar-page">
+        <div className="calendar-container">
+          <div className="calendar-header">
+            <h1>Calendar</h1>
+            <p>Click on any day with an entry to view your diary</p>
+          </div>
+          <div className="calendar-content">
+            <div className="calendar">
+              <CalendarUnit
+                className="react-calendar"
+                onChange={() => {}}
+                locale="en-US"
+                next2Label={null}
+                prev2Label={null}
+                nextLabel={<ChevronRight className="arrow"/>}
+                prevLabel={<ChevronLeft className="arrow"/>}
+                showNeighboringMonth={false}
+                maxDate={new Date()}
+                minDate={new Date('2021-01-01')}
+              />
+            </div>
+            <div className="calendar-diary">
+
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
